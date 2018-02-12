70272a80e3778171a4da3efd893066637bec7d25
sampsong <sampsong@qq.com>
2018-02-12 11:53:54 +0800

    重新定义bootstrap

--- src/styles/index.vue
+++ src/styles/index.vue
@@ -2,3 +2,8 @@
 /** this component is used only for building vux.css **/
 @import './zkui.less';
 </style>
+
+<style lang="scss">
+/** this component is used only for building vux.css **/
+@import './bootstrap/bootstrap';
+</style>
