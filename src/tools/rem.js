4a417308c6a84fd12c94529396e896652c3ababa
sampsong <sampsong@qq.com>
2018-02-14 19:37:33 +0800

    更新CSS样式

--- src/tools/rem.js
+++ src/tools/rem.js
@@ -4,7 +4,7 @@
   var recalc = function() {
     var clientWidth = docEl.clientWidth
     if (!clientWidth) return
-    docEl.style.fontSize = 16 * (clientWidth / 320) + 'px'
+    docEl.style.fontSize = 14 * (clientWidth / 320) + 'px'
   }
   if (!doc.addEventListener) return
   win.addEventListener(resizeEvt, recalc, false)
