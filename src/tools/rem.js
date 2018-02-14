cc45b557bac19debcb790f66f60560e0d640b41c
sampsong <sampsong@qq.com>
2018-02-14 17:38:00 +0800

    更新CSS样式

--- src/tools/rem.js
+++ src/tools/rem.js
@@ -1,10 +1,10 @@
-;(function (doc, win) {
+;(function(doc, win) {
   var docEl = doc.documentElement
   var resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize'
-  var recalc = function () {
+  var recalc = function() {
     var clientWidth = docEl.clientWidth
     if (!clientWidth) return
-    docEl.style.fontSize = 14 * (clientWidth / 320) + 'px'
+    docEl.style.fontSize = 12 * (clientWidth / 320) + 'px'
   }
   if (!doc.addEventListener) return
   win.addEventListener(resizeEvt, recalc, false)
